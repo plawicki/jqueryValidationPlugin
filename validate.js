87b7b4362b864e40b31d7bcfd37e8b695b11180e
Patryk <patlaw96@gmail.com>
2015-05-06 19:30:37 +0200

    refactor

--- validate.js
+++ validate.js
@@ -1,10 +1,14 @@
+/* global jQuery */
 (function($){
 	
+  "use strict";
+  
 	$.fn.validate = function(settings, styles){
 		var standartSettings = {
 			styles:{
 				valid : 'valid',
-				invalid: 'invalid'
+				invalid: 'invalid',
+				password: 'password'
 			},
 			selectors:{
 				name: '#name',
@@ -20,66 +24,117 @@
 			},
 			patterns:{
 				zip: /^\d{2}-\d{3}$/g,
-				email: /\S+@\S+\.\S+/g
+				email: /\S+@\S+\.\S+/
 			}
-		}
+		};
 		var $that = $(this);
 
-		var validate = function($input, pattern) {
-			var iValue = $input.val();
-			if(typeof pattern === 'string' || pattern instanceof String) {
-				if(pattern === "zip" && standartSettings.patterns.zip.test(iValue)) {
-					return true;
-				}
-				if(pattern === "email" && standartSettings.patterns.email.test(iValue)) {
-					return true;
-				}
-			} else if(pattern instanceof RegExp) {
-				if(pattern.test(iValue)) {
-					return true;
-				}
-			} else if(pattern instanceof Object) {
-				if(standartSettings.patterns.zip.test(iValue)) {
+		var paintField = function($input, correct) {
+			var classs = correct ? (styles && styles.valid || standartSettings.styles.valid) : (styles && styles.invalid || standartSettings.styles.invalid);
 
-					if(!pattern.url) {
-						console.error("Cannot find url field in zip object")
-						return true;
-					}
+			$input.removeClass(styles && styles.valid || standartSettings.styles.valid)
+				.removeClass(styles && styles.invalid || standartSettings.styles.invalid)
+				.addClass(classs);
 
-					var $autoAdressDiv = $input.parent().children(pattern.address || standartSettings.selectors.address);
+			return correct;
+		};
+
+		var validateZip = function($input) {
+			paintField(standartSettings.patterns.zip.test($input, $input.val()));
+		};
+
+		var validateZipAutocomplete = function($input, pattern) {
+			var $autoAdressDiv = $input.parent().children(pattern.wrapper.selector || standartSettings.selectors.address);
 					
-					$.ajax({
-						url: pattern.url,
-						done: function(){
-							console.log("ASDASD");
-						},
-						fail: function(){
-							$("body").css("background", "black");
-						},
-						always: function(){
-							$("body").append("<img></img>");
-							console.log($autoAdressDiv);
-						}
-					})
+			$.ajax({
+				url: pattern.url,
+				dataType: "json"
+			}).done(function(json) {
+				var zip = json[$input.val()];
 
-					return true;
+				if(paintField($input, zip)) {
+					$.each(pattern.wrapper, function(key, val){
+						$($autoAdressDiv).children(val.selector || standartSettings.selectors.street).val(
+							zip[val.jsonField || val]
+						);
+					});
+				}
+			});
+		};
+
+		var isPasswordStrong = function(value) {
+			return (value > 1.7) ? true : false;
+		};
+
+		var countStrength = function(password) {
+			var strength = 0;
+			var weightSmall = 1, weightBig = 2, weightSpecial = 3;
+
+			for(var c=0; c<password.length; c++) {
+				var character = password.charCodeAt(c);
+				if(character > 96 && character < 123) {
+					strength += weightSmall;
+				} else if (character > 64 && character < 91) {
+					strength += weightBig;
+				} else {
+					strength += weightSpecial;
 				}
 			}
-			return false;
-		}
+
+			return strength / password.length;
+		};
+
+		var validatePassword = function($input) {
+			var passwordStr = countStrength($input.val());
+
+			paintField($input, isPasswordStrong(passwordStr));
+		};
+
+		var validateEmail = function($input) {
+			console.log(standartSettings.patterns.email.test($input.val()))
+			paintField($input, standartSettings.patterns.email.test($input.val()));
+		};
+
+		var validateByRegexp = function($input, pattern) {
+			paintField($input, pattern.test($input.value));
+		};
+
+		var validateCore = function($input, pattern) {
+			switch(pattern.constructor) {
+				case String: 
+					validateStrings($input, pattern);
+					break;
+				case RegExp:
+					validateByRegexp($input, pattern);
+					break;
+				case Object: 
+					validateZipAutocomplete($input, pattern);
+					break;
+			}
+		};
+
+		var validateStrings = function($input, pattern) {
+			switch(pattern) {
+				case "zip":
+					validateZip($input);
+					break;
+				case "email":
+					validateEmail($input);
+					break;
+				case "password":
+					validatePassword($input);
+					break;
+			}
+		};
 
 		$.each(settings, function(input, value) {
 			var $input = $that.children(input);
 			$input.on('input', function() {
-				if(validate($input, value)) {
-					$input.addClass(styles && styles.valid ? styles.valid : standartSettings.styles.valid);
-					$input.removeClass(styles && styles.invalid ? styles.invalid : standartSettings.styles.invalid);
-				} else {
-					$input.addClass(styles && styles.invalid ? styles.invalid : standartSettings.styles.invalid);
-					$input.removeClass(styles && styles.valid ? styles.valid : standartSettings.styles.valid);
-				}
+				validateCore($input, value);
 			});
 		});
-	}
+
+		return this;
+	};
 	
 })(jQuery);
